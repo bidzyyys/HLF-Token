a58780d797e844849a3a22a016c696fca44f663e
Daniel Bigos <daniel.bigos@icloud.com>
2020-04-28 13:48:30 +0200

    #1 | feat: balanceOf

--- src/main/java/com/bidzyyys/token/TokenContract.java
+++ src/main/java/com/bidzyyys/token/TokenContract.java
@@ -1,7 +1,12 @@
 package com.bidzyyys.token;
 
+import com.owlike.genson.Genson;
+import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.ContractInterface;
 import org.hyperledger.fabric.contract.annotation.*;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import java.math.BigInteger;
 
 /**
  * Java implementation of the ERC20 Token on Hyperledger Fabric
@@ -20,4 +25,20 @@ import org.hyperledger.fabric.contract.annotation.*;
                         url = "https://github.com/bidzyyys")))
 @Default
 public final class TokenContract implements ContractInterface {
+    private final Genson genson = new Genson();
+
+    /**
+     * Retrieves balance of specified account.
+     *
+     * @param ctx     the transaction context
+     * @param address address of the account
+     * @return balance of the account, 0 if unknown
+     */
+    @Transaction()
+    public BigInteger balanceOf(final Context ctx, final String address) {
+        ChaincodeStub stub = ctx.getStub();
+        String accountState = stub.getStringState(address);
+        return accountState.isEmpty() ? BigInteger.ZERO : genson.deserialize(accountState, BigInteger.class);
+    }
+
 }
--- src/test/java/com/bidzyyys/token/TokenContractTest.java
+++ src/test/java/com/bidzyyys/token/TokenContractTest.java
@@ -1,11 +1,45 @@
 package com.bidzyyys.token;
 
+import com.owlike.genson.Genson;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.shim.ChaincodeStub;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class TokenContractTest {
+    private final Genson genson = new Genson();
 
     @Test
-    public void sampleTest() {
-        assert 2 == 2;
+    public void balanceOfUnknownAddress() {
+        TokenContract contract = new TokenContract();
+        Context ctx = mock(Context.class);
+        ChaincodeStub stub = mock(ChaincodeStub.class);
+        when(ctx.getStub()).thenReturn(stub);
+
+        String address = "1234";
+        when(stub.getStringState(address)).thenReturn("");
+
+        BigInteger amount = contract.balanceOf(ctx, address);
+
+        assertThat(amount).isEqualTo(BigInteger.ZERO);
+    }
+
+    @Test
+    public void balanceOfNonZeroAssets() {
+        TokenContract contract = new TokenContract();
+        Context ctx = mock(Context.class);
+        ChaincodeStub stub = mock(ChaincodeStub.class);
+        when(ctx.getStub()).thenReturn(stub);
+
+        String address = "1234";
+        BigInteger balance = new BigInteger("69");
+        when(stub.getStringState(address)).thenReturn(genson.serialize(balance));
+
+        assertThat(contract.balanceOf(ctx, address)).isEqualTo(balance);
     }
 }
